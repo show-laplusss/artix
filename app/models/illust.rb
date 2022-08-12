eb568db6baf2e88a187111a0290aca8bd34ad37f
EC2 Default User <106435322+show->
2022-08-12 21:43:40 +0000

    controll

--- app/models/illust.rb
+++ app/models/illust.rb
@@ -1,4 +1,11 @@
 class Illust < ApplicationRecord
     has_one_attached :image
     belongs_to :user
+    has_many :favorites, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    
+    def favorited_by?(user)
+        favorites.exists?(user_id: user.id)
+    end
+    
 end
