16a62b7139b6c93c1e3fdeb2b1821288bf35cdc3
EC2 Default User <106435322+show->
2022-08-15 13:02:50 +0000

    message controller

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -1,2 +1,16 @@
 class Public::RoomsController < ApplicationController
+    before_action :authenticate_user!
+    def create
+        room = Room.new
+        current = EntryRoom.new(user_id: current_user.id, room_id: room.id)
+        another = EntryRoom.new(user_id: params[:entry][:user_id], room_id: room.id)
+        redirect_to room_path(room)
+    end
+    def index
+        current = current_user.EntryRoom
+        my_room_id = []
+        current.each do |entryroom|
+            my_room_id << entryroom.room.id
+        end
+    end
 end
