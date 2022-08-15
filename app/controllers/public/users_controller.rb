2684e6c1fcb84fe83804a265eac2d9a611e5769d
EC2 Default User <106435322+show->
2022-08-15 12:51:06 +0000

    message

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,6 +3,23 @@ class Public::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @current = EntryRoom.where(user_id: current_user.id)
+    @another = EntryRoom.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current.each do |current|
+        @another.each do |another|
+          if current.room_id == another.room_id
+            @is_room = true
+            @room_id = current.room_id
+          end
+        end
+      end
+      unless @is_room
+        @room = Room.new
+        @entry = EntryRoom.new
+      end
+    end
   end
 
   def edit
