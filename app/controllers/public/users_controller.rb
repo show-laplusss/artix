6fe9dc9a43a3910df9797b10883aa4fad41a6b8a
EC2 Default User <106435322+show->
2022-08-15 12:24:52 +0000

    bookmark2

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,9 +3,7 @@ class Public::UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @current_entry = EntryRoom.where(user_id: current_user.id)
-    @another = EntryRoom.where(user_id: @user_id)
+
   end
 
   def edit
