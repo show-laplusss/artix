cae224ca6f829c0b94bb75146e42608d7e354b66
EC2 Default User <106435322+show->
2022-08-17 10:07:03 +0000

    tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_08_12_214727) do
-z
+ActiveRecord::Schema.define(version: 2022_08_17_094627) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -84,6 +82,11 @@ z
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "illust_tags", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "illusts", force: :cascade do |t|
     t.integer "user_id"
     t.string "image_tag"
@@ -102,6 +105,12 @@ z
 
   create_table "rooms", force: :cascade do |t|
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
